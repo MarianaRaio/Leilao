b327b5c48f97ed5733c236d218af55dd438fb532
Mariana Raio <marianaraiooo@gmail.com>
2024-06-26 12:43:04 -0300

    Update ProdutosDAO.java

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -20,7 +20,7 @@ public class ProdutosDAO {
 
     public void cadastrarProduto(ProdutosDTO produto) {
 
-        //tudo salvo no banco
+        
         conn = new conectDAO().connectDB();
         try {
             prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES (?,?,?)", Statement.RETURN_GENERATED_KEYS);
